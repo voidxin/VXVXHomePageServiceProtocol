29ad0da16a87e25e11dabcb30092ff681d90c85d
voidxin <zhangxin409999@163.com>
2017-04-10 10:55:32 +0800

    Initial Commit

---
+++ VXVXHomePageServiceProtocol/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VXVXHomePageServiceProtocol
+//
+//  Created by voidxin on 17/4/10.
+//  Copyright © 2017年 voidxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VXVXHomePageServiceProtocol/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  VXVXHomePageServiceProtocol
+//
+//  Created by voidxin on 17/4/10.
+//  Copyright © 2017年 voidxin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
